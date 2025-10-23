57252b06834bb23f35d8a5f4b96e108a8cfa9b16
Adam Burgett <asburgett@gmail.com>
2025-10-22 21:29:58 -0400

    Added rotate_point function to triangle.py which allows for calculating a new location of an existing point with a translation.  This should be useful in plotting multiple triangles.  Added compute.py with the Compute class that will be used for uniform calculation methods such as calculating the number of significant digits are valid for a small data set.  Version bump

---
+++ src/classes/utils/compute.py
@@ -0,0 +1,13 @@
+import math
+
+class Compute:
+    def __init__(self):
+        self.create_run_folder = True
+
+    def calculate_significant_digits_from_data(self, data):
+        # push vars to the function, return the maximum # of digits to round to, minimum accuracy by rules
+        print(f"No calculations performed")
+
+    def calculate_significant_digits_from_list(self, data):
+        # process a list for values, return the maximum # of digits to round to, minimum accuracy by rules
+        print(f"No calculations performed")
